13ddd691c907b7019b2112ebc0427b94fa1145ce
Roman Ožana <roman@ozana.cz>
2022-10-20 11:00:42 +0200

    Simplify toOpenApi() code

--- src/to-open-api.js
+++ src/to-open-api.js
@@ -4,11 +4,16 @@
  * @returns {{format: (string), type: string, example: number}|{type: string, items: {type: ("undefined"|"object"|"boolean"|"number"|"string"|"function"|"symbol"|"bigint")}}|{format: string, type: string}|{}|{type: string, example: string}|{type: string, example: number}|{type: string, example: boolean}|{format: string, type: string, example: number}}
  */
 import {getType} from './get-type.js';
+import {getFormat} from './get-format.js';
 
 export function toOpenApi(item) {
 	const oa = {};
 
-	switch (getType(item)) {
+	const format = getFormat(item);
+	const type = getType(item);
+	const example = item;
+
+	switch (type) {
 		case 'object':
 			oa.type = 'object';
 			oa.properties = {};
@@ -18,22 +23,15 @@ export function toOpenApi(item) {
 
 			break;
 		case 'array':
-			return {
-				type: 'array',
-				items: toOpenApi(item[0]),
-			};
+			return {type, items: toOpenApi(item[0])};
 		case 'integer':
-			if (item < 2_147_483_647 && item > -2_147_483_647) {
-				return {type: 'integer', format: 'int32', example: item};
-			}
-
-			return {type: 'integer', format: Number.isSafeInteger(item) ? 'int64' : 'unsafe', example: item};
+			return {type, format, example};
 		case 'number':
-			return {type: 'number', example: item};
+			return {type, example};
 		case 'boolean':
-			return {type: 'boolean', example: item};
+			return {type, example};
 		case 'string':
-			return {type: 'string', example: item};
+			return format ? {type, format, example} : {type, example};
 		default:
 			return {type: 'string', format: 'nullable'};
 	}
