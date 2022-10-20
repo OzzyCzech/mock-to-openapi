844ee0fd4a45b022132bc1f24f8d7493627351be
Roman Ožana <roman@ozana.cz>
2022-10-20 11:00:27 +0200

    Better Integer detection

--- src/get-type.js
+++ src/get-type.js
@@ -13,7 +13,7 @@ export function getType(item) {
 	}
 
 	if (typeof item === 'number') {
-		return item % 1 === 0 ? 'integer' : 'number';
+		return Number.isInteger(item) ? 'integer' : 'number';
 	}
 
 	if (Object.prototype.toString.call(item) === '[object Array]') {
